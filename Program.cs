4b396f8fd817c39b26f30a58bc748bfe07402491
William Sellinger <wsellinger@gmail.com>
2026-05-22 09:27:36 -0400

    Implement Post Scores using Redis

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+//Add Redis
+builder.Services.AddSingleton<IConnectionMultiplexer>(
+    ConnectionMultiplexer.Connect("localhost:6379"));
 
 var app = builder.Build();
 
@@ -13,4 +19,15 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+//Map Requests
+app.MapPost("/leaderboard/{game}/score", async (string game, ScoreEntry entry, IConnectionMultiplexer redis) =>
+{
+    var db = redis.GetDatabase();
+    await db.SortedSetAddAsync($"leaderboard:{game}", entry.Player, entry.Score);
+    return Results.Ok();
+});
+
 app.Run();
+
+record ScoreEntry(string Player, double Score);
