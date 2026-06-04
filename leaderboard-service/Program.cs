20c1ccb0a263d022995d75293026cdde83284ee8
William Sellinger <wsellinger@gmail.com>
2026-06-04 11:16:40 -0400

    Added scalar support so launched browser window is now useful

--- leaderboard-service/Program.cs
+++ leaderboard-service/Program.cs
@@ -1,3 +1,4 @@
+using Scalar.AspNetCore;
 using StackExchange.Redis;
 
 //=== Constants
@@ -22,6 +23,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
