5566271004b5acc626790be10916436f16f042eb
William Sellinger <wsellinger@gmail.com>
2026-06-06 12:21:25 -0400

    Auth testing project implemented with initial test

---
+++ Didakt.Api.Auth.Tests/EndpointTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Didakt.Api.Auth.Tests;
+
+public class EndpointTests
+{
+
+    public EndpointTests() {}
+
+    [Fact]
+    public async Task PostScore_Given_ValidInput_Returns_OK()
+    {
+        //Arrange
+
+        //Act
+        var result = await Endpoints.PostRegister();
+
+        //Assert
+        Assert.IsType<Ok>(result);
+    }
+}
