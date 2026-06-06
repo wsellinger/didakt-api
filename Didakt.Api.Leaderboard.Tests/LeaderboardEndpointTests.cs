c9eda74ad27ccff966eeacaf43ba84436c460858
William Sellinger <wsellinger@gmail.com>
2026-06-06 11:55:28 -0400

    Leaderboard testing project implemented with initial test

---
+++ Didakt.Api.Leaderboard.Tests/LeaderboardEndpointTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Moq;
+using StackExchange.Redis;
+
+namespace Didakt.Api.Leaderboard.Tests;
+
+public class LeaderboardEndpointTests
+{
+    private readonly Mock<IDatabase> _database;
+    private readonly Mock<IConnectionMultiplexer> _connection;
+
+    public LeaderboardEndpointTests()
+    {
+        _database = new Mock<IDatabase>();
+        _connection = new Mock<IConnectionMultiplexer>();
+
+        _connection.Setup(x => x.GetDatabase(It.IsAny<int>(), It.IsAny<object>()))
+            .Returns(_database.Object);
+    }
+
+    [Fact]
+    public async Task PostScore_Given_ValidInput_Returns_OK()
+    {
+        //Arrange
+        var gameName = "testGame";
+        var playerName = "testPlayer";
+        var scoreAmount = 1234;
+        var postScoreRequest = new Endpoints.PostScoreRequest(playerName, scoreAmount);
+
+        var expectedResult = true;
+
+        _database.Setup(x => x.SortedSetAddAsync(It.IsAny<RedisKey>(), It.IsAny<RedisValue>(), It.IsAny<double>(), It.IsAny<SortedSetWhen>(), It.IsAny<CommandFlags>()))
+            .ReturnsAsync(expectedResult);
+
+        //Act
+        var result = await Endpoints.PostScore(gameName, postScoreRequest, _connection.Object);
+
+        //Assert
+        Assert.True(expectedResult);
+        Assert.IsType<Ok>(result);
+    }
+}
