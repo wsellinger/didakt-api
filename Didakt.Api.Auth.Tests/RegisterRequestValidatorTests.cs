7a55b85d334e60512322efb3a12c679a0fb1d2b6
William Sellinger <wsellinger@gmail.com>
2026-06-07 18:11:36 -0400

    RegisterRequestValidator Unit Tests Implemented

---
+++ Didakt.Api.Auth.Tests/RegisterRequestValidatorTests.cs
@@ -0,0 +1,52 @@
+﻿using Didakt.Api.Auth.Models.Requests;
+using Didakt.Api.Auth.Validators;
+
+namespace Didakt.Api.Auth.Tests
+{
+    public class RegisterRequestValidatorTests
+    {
+        public RegisterRequestValidatorTests() { }
+
+        [Theory]
+        [InlineData("123", "123456789012")] //Min Size
+        [InlineData("12345678901234567890", "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678")] //Max Size
+        [InlineData("a-B_3", "!@#$%^&*()-_")] //Allowed Chars
+        public void Validate_ValidInput_IsValid(string userName, string password)
+        {
+            //Arrange
+            var request = new RegisterRequest(userName, password);
+            var validator = new RegisterRequestValidator();
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            Assert.True(result.IsValid);
+        }
+
+        [Theory]
+        [InlineData("", "123456789012")] //Empty userName
+        [InlineData("12", "")] //Empty Password
+        [InlineData("12", "123456789012")] //Short userName
+        [InlineData("123", "12345678901")] //Short password
+        [InlineData("123456789012345678901", "123456789012")] //Long userName
+        [InlineData("123", "123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789")] //Long password
+        [InlineData("a@c", "12345678901")] //Invalid character in userName
+        [InlineData("-abc", "12345678901")] //Invalid character at start of userName
+        [InlineData("_abc", "12345678901")] //Invalid character at start of userName
+        [InlineData("abc-", "12345678901")] //Invalid character at end of userName
+        [InlineData("abc_", "12345678901")] //Invalid character at end of userName
+        public void Validate_InvalidInput_IsNotValid(string userName, string password)
+        {
+            //Arrange
+            var request = new RegisterRequest(userName, password);
+            var validator = new RegisterRequestValidator();
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            Assert.False(result.IsValid);
+        }
+    }
+}
