6233cebccb7199ef57a5136729f9681a04690237
William Sellinger <wsellinger@gmail.com>
2026-06-11 12:49:40 -0400

    Start bat running check logic fixed

--- Didakt.Api.Auth/start-postgres.bat
+++ Didakt.Api.Auth/start-postgres.bat
@@ -1,10 +1,10 @@
 @echo off
-docker ps --filter "name=didakt-postgres" --filter "status=running" -q | find /i "." >nul 2>&1
-if errorlevel 1 (
-    docker ps -a --filter "name=didakt-postgres" -q | find /i "." >nul 2>&1
-    if errorlevel 1 (
-        docker run -d --name didakt-postgres -p 5432:5432 -e POSTGRES_USER=didakt_user -e POSTGRES_PASSWORD=didakt_dev -e POSTGRES_DB=didakt postgres:17
-    ) else (
-        docker start didakt-postgres
-    )
+for /f %%i in ('docker ps --filter "name=didakt-postgres" --filter "status=running" -q') do set RUNNING=%%i
+if defined RUNNING exit /b 0
+
+for /f %%i in ('docker ps -a --filter "name=didakt-postgres" -q') do set EXISTS=%%i
+if defined EXISTS (
+    docker start didakt-postgres
+) else (
+    docker run -d --name didakt-postgres -p 5432:5432 -e POSTGRES_USER=didakt_user -e POSTGRES_PASSWORD=didakt_dev -e POSTGRES_DB=didakt postgres:17
 )
--- Didakt.Api.Leaderboard/start-redis.bat
+++ Didakt.Api.Leaderboard/start-redis.bat
@@ -1,10 +1,10 @@
 @echo off
-docker ps --filter "name=didakt-redis" --filter "status=running" -q | find /i "." >nul 2>&1
-if errorlevel 1 (
-    docker ps -a --filter "name=didakt-redis" -q | find /i "." >nul 2>&1
-    if errorlevel 1 (
-        docker run -d --name didakt-redis -p 6379:6379 redis
-    ) else (
-        docker start didakt-redis
-    )
+for /f %%i in ('docker ps --filter "name=didakt-redis" --filter "status=running" -q') do set RUNNING=%%i
+if defined RUNNING exit /b 0
+
+for /f %%i in ('docker ps -a --filter "name=didakt-redis" -q') do set EXISTS=%%i
+if defined EXISTS (
+    docker start didakt-redis
+) else (
+    docker run -d --name didakt-redis -p 6379:6379 redis
 )
