904b5531fd6a57eba0b80ee8b94b7abeca721063
William Sellinger <wsellinger@gmail.com>
2026-06-25 15:50:29 -0400

    Updated "game" to "category" in leaderboard service path for clarity on purpose

--- Didakt.Api.Leaderboard.Tests/EndpointTests.cs
+++ Didakt.Api.Leaderboard.Tests/EndpointTests.cs
@@ -24,7 +24,7 @@ public class EndpointTests
     public async Task PostScore_Given_ValidInput_Returns_OK()
     {
         //Arrange
-        var gameName = "testGame";
+        var categoryName = "testCategory";
         var playerName = "testPlayer";
         var scoreAmount = 1234;
         var postScoreRequest = new PostScoreRequest(playerName, scoreAmount);
@@ -32,10 +32,10 @@ public class EndpointTests
         _database.Setup(x => x.SortedSetAddAsync(It.IsAny<RedisKey>(), It.IsAny<RedisValue>(), It.IsAny<double>()));
 
         //Act
-        var result = await EndpointMethods.PostScore(gameName, postScoreRequest, _connection.Object);
+        var result = await EndpointMethods.PostScore(categoryName, postScoreRequest, _connection.Object);
 
         //Assert
-        _database.Verify(x => x.SortedSetAddAsync(It.Is<RedisKey>(x => x.ToString().Contains(gameName)), playerName, scoreAmount), Times.Once);
+        _database.Verify(x => x.SortedSetAddAsync(It.Is<RedisKey>(x => x.ToString().Contains(categoryName)), playerName, scoreAmount), Times.Once);
 
         Assert.IsType<Ok>(result);
     }
--- Didakt.Api.Leaderboard/Endpoints/Endpoints.cs
+++ Didakt.Api.Leaderboard/Endpoints/Endpoints.cs
@@ -12,7 +12,7 @@ internal static class Endpoints
     {
         internal WebApplication MapEndpoints()
         {
-            var group = app.MapGroup("/leaderboard/{game}/");
+            var group = app.MapGroup("/leaderboard/{category}/");
             group.MapPost("score", EndpointMethods.PostScore).RequireAuthorization();
             group.MapGet("score", EndpointMethods.GetScore);
             group.MapGet("top", EndpointMethods.GetTopPlayers);
@@ -29,27 +29,27 @@ internal static class EndpointMethods
 
     //Post Score
 
-    internal static async Task<IResult> PostScore(string game, PostScoreRequest entry, IConnectionMultiplexer redis)
+    internal static async Task<IResult> PostScore(string category, PostScoreRequest entry, IConnectionMultiplexer redis)
     {
         var db = redis.GetDatabase();
-        await db.SortedSetAddAsync($"{KeyBase}{game}", entry.Player, entry.Score);
+        await db.SortedSetAddAsync($"{KeyBase}{category}", entry.Player, entry.Score);
         return Results.Ok();
     }
 
     //Get Score
-    internal static async Task<double> GetScore(string game, string player, IConnectionMultiplexer redis)
+    internal static async Task<double> GetScore(string category, string player, IConnectionMultiplexer redis)
     {
         var db = redis.GetDatabase();
-        double? score = await db.SortedSetScoreAsync($"{KeyBase}{game}", player);
+        double? score = await db.SortedSetScoreAsync($"{KeyBase}{category}", player);
         return score ?? 0;
     }
 
     //Get Top Players
 
-    internal static async Task<GetTopPlayersResponse[]> GetTopPlayers(string game, long count, IConnectionMultiplexer redis)
+    internal static async Task<GetTopPlayersResponse[]> GetTopPlayers(string category, long count, IConnectionMultiplexer redis)
     {
         var db = redis.GetDatabase();
-        SortedSetEntry[] entries = await db.SortedSetRangeByRankWithScoresAsync($"{KeyBase}{game}", 0, count - 1, Order.Descending);
+        SortedSetEntry[] entries = await db.SortedSetRangeByRankWithScoresAsync($"{KeyBase}{category}", 0, count - 1, Order.Descending);
         var rank = 0;
         return [.. entries.Select(x => new GetTopPlayersResponse(++rank, x.Element.ToString(), x.Score))];
     }
